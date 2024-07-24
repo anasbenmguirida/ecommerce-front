1fca86b0698d9d69f9e536072f368f2572027927
anasbenmguirida <anasbenmki@gmail.com>
2024-07-24 23:31:16 +0100

    consuming product API

--- front/ecommercefront/src/app/app.component.ts
+++ front/ecommercefront/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { CartComponent } from './cart/cart.component';
 import { HeaderComponent } from "./header/header.component";
+import { HttpClientModule } from '@angular/common/http';
+import { ProduitComponent } from "./produit/produit.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomepageComponent, CartComponent, HeaderComponent],
+  imports: [RouterOutlet, HomepageComponent, CartComponent, HeaderComponent, HttpClientModule,ProduitComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
@@ -15,3 +17,4 @@ import { HeaderComponent } from "./header/header.component";
 export class AppComponent {
   title = 'ecommercefront';
 }
+
--- front/ecommercefront/src/app/produit/produit.component.ts
+++ front/ecommercefront/src/app/produit/produit.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { ProductService } from '../product.service';
+import { NgModule } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-produit',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './produit.component.html',
   styleUrl: './produit.component.css'
 })
-export class ProduitComponent {
+export class ProduitComponent implements OnInit{
+  products: any[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(
+      data => this.products = data,
+      error => console.error('Error fetching products', error)
+    );
+  }
 }
