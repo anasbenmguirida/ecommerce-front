d103ae752dcbf40b0c45a39aae20bea812eb2e7d
anasbenmguirida <anasbenmki@gmail.com>
2024-09-09 17:56:14 +0100

    adding the thank you page

--- front/ecommercefront/src/app/payment/payment.component.ts
+++ front/ecommercefront/src/app/payment/payment.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+import { NgFor } from '@angular/common';
+import { RouterModule , RouterLink , RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [],
+  imports: [NgFor , RouterModule],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
 export class PaymentComponent {
+produits:any[]=[] ; 
+price:number=0 ; 
+constructor(private cartService:CartService){}
+ngOnInit(): void {
+  this.produits=this.cartService.getItems() ; 
+  this.price=this.cartService.getTotalPrice() ; 
+  
+}
 
 }
